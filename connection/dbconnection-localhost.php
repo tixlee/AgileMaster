f20e0b61cbc6026596ccb182e8b3df6417ef698b
ahmedtarek100089038 <55531401+ahmedtarek100089038@users.noreply.github.com>
2020-11-18 01:13:51 +0800

    upload files for live server and localhot server

--- connection/dbconnection-localhost.php
+++ connection/dbconnection-localhost.php
@@ -1,8 +1,8 @@
 <?php
 $hostname_dbconnection = "localhost"; 
 $database_dbconnection = "am";
-$username_dbconnection = "fyp";
-$password_dbconnection = "aGile@2020Master";
+$username_dbconnection = "root";
+$password_dbconnection = "";
 
 
 $conn = mysqli_connect($hostname_dbconnection, $username_dbconnection, $password_dbconnection, $database_dbconnection); 
